efe119d14412d384125fb1ae2e6a40c9d33f9bbd
bo_zhang <bo_zhang1993@163.com>
2017-10-17 19:12:10 +0800

    Add Grab&ProceduralTexture

---
+++ UnityShaderStudy/Assets/Scripts/ProceduralTextureGeneration.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEditor.Experimental.Animations;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+[ExecuteInEditMode]
+public class ProceduralTextureGeneration : MonoBehaviour {
+
+    public Material material = null;
+    #region Material properties
+    [SerializeField,SetProperty("textureWidth")]
+    private int m_textureWidth = 512;
+    public int textureWidth {
+        get {
+            return m_textureWidth;
+        }
+        set {
+            m_textureWidth = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("backgroundColor")]
+    private Color m_backgroundColor = Color.white;
+    public Color backgroundColor {
+        get {
+            return m_backgroundColor;
+        }
+        set {
+            m_backgroundColor = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("circleColor")]
+    private Color m_circleColor = Color.yellow;
+    public Color circleColor {
+        get {
+            return m_circleColor;
+        }
+        set {
+            m_circleColor = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("blurFactor")]
+    private float m_blurFactor = 2.0f;
+    public float blurFactor {
+        get {
+            return m_blurFactor;
+        }
+        set {
+            m_blurFactor = value;
+            _UpdateMaterial();
+        }
+    }
+    #endregion
+
+    private Texture2D m_generateTexture = null;
+
+
+    // Use this for initialization
+    void Start () {
+        if (material == null)
+        {
+            Renderer renderers = gameObject.GetComponent<Renderer>();
+            if (renderers == null)
+            {
+                Debug.LogWarning("Cannot find a renderer.");
+                return;
+            }
+            material = GetComponent<Renderer>().sharedMaterial;
+        }
+        _UpdateMaterial();
+    }
+	
+	// Update is called once per frame
+    private void _UpdateMaterial() {
+        if (material != null)
+        {
+            m_generateTexture = _GenerateProceduralTexture();
+            material.SetTexture("_MainTex",m_generateTexture);
+        }
+    }
+
+    private Texture2D _GenerateProceduralTexture()
+    {
+        Texture2D proceduralTexture=new Texture2D(textureWidth,textureWidth);
+
+        //定义圆与圆之间的距离
+        float circleInterval = textureWidth/4.0f;
+        //定义圆的半径
+        float radius = textureWidth/10.0f;
+        //定义模糊系数
+        float edgeBlur = 1.0f/blurFactor;
+
+        for (int w = 0; w < textureWidth; w++)
+        {
+            for (int h = 0; h < textureWidth; h++)
+            {
+                Color pixel = backgroundColor;
+
+                //绘制9个圆
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        //计算当前所绘制圆的位置
+                        Vector2 circleCenter=new Vector2(circleInterval*(i+1),circleInterval*(j+1));
+                        //计算当前像素与圆边界的距离
+                        float dist = Vector2.Distance(new Vector2(w, h), circleCenter)-radius;
+
+                        //模糊圆的边界
+                        Color color = _MixColor(circleColor, new Color(pixel.r,pixel.g,pixel.b,0.0f),Mathf.SmoothStep(0f,1f,dist*edgeBlur));
+
+                        //与之前得到的颜色混合
+                        pixel = _MixColor(pixel, color, color.a);
+                    }
+                }
+                proceduralTexture.SetPixel(w,h,pixel);
+            }
+        }
+        proceduralTexture.Apply();
+        return proceduralTexture;
+    }
+
+
+    private Color _MixColor(Color color0, Color color1, float mixFactor)
+    {
+        Color mixColor = Color.white;
+        mixColor.r = Mathf.Lerp(color0.r, color1.r, mixFactor);
+        mixColor.g = Mathf.Lerp(color0.g, color1.g, mixFactor);
+        mixColor.b = Mathf.Lerp(color0.b, color1.b, mixFactor);
+        mixColor.a = Mathf.Lerp(color0.a, color1.a, mixFactor);
+        return mixColor;
+    }
+}
---
+++ UnityShaderStudy/Assets/Shader/Chapter10/Chapter10_GlassRefraction.shader
@@ -0,0 +1,91 @@
+﻿Shader "Custom/Chapter10_GlassRefraction" {
+	Properties{
+		_MainTex("Main Tex",2D)="white"{}
+		_BumpTex("Bump Tex",2D)="bump"{}
+		_CubeMap("Cube Map",Cube)="_Skybox"{}
+		_Distortion("Distortion",Range(0,100))=10
+		_RefractAmount("Refract Amount",Range(0.0,1.0))=1.0
+	}
+	SubShader{
+		Tags{"Queue"="Transparent" "RenderType"="Opaque"}
+		//指定渲染队列为"Transparent"，确保所有不透明物体先渲染完成
+		GrabPass {"_RefractionTex"}
+		//声明GrabPass 该Pass会将屏幕抓取图像存储到名为"_RefractionTex"的纹理中
+		Pass{
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag 
+
+				#include "UnityCG.cginc"
+
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+				sampler2D _BumpTex;
+				float4 _BumpTex_ST;
+				samplerCUBE _CubeMap;
+				float _Distortion;
+				float _RefractAmount;
+				sampler2D _RefractionTex;   //存储GrabPass抓取的屏幕图像
+				float4 _RefractionTex_TexelSize;  //得到屏幕图像的纹素值，在做偏移计算时使用
+
+				struct a2v{
+					float4 vertex:POSITION;
+					float3 normal:NORMAL;
+					float4 tangent:TANGENT;
+					float4 texcoord:TEXCOORD0;
+				};
+
+				struct v2f{
+					float4 pos:SV_POSITION;
+					float4 uv:TEXCOORD0;
+					float4 scrPos:TEXCOORD1;
+					float4 TtoW0:TEXCOORD2;
+					float4 TtoW1:TEXCOORD3;
+					float4 TtoW2:TEXCOORD4;
+				};
+
+				v2f vert(a2v v){
+					v2f o;
+					o.pos=UnityObjectToClipPos(v.vertex);
+					o.scrPos=ComputeGrabScreenPos(o.pos);
+					o.uv.xy=TRANSFORM_TEX(v.vertex,_MainTex);
+					o.uv.zw=TRANSFORM_TEX(v.vertex,_BumpTex);
+
+					float3 worldPos=mul(unity_ObjectToWorld,v.vertex).xyz;
+
+					fixed3 worldNormal=UnityObjectToWorldNormal(v.normal);
+					fixed3 worldTangent=UnityObjectToWorldDir(v.tangent.xyz);
+					fixed3 worldBinormal=cross(worldNormal,worldTangent)*v.tangent.w;
+
+					o.TtoW0=(worldTangent.x,worldBinormal.x,worldNormal.x,worldPos.x);
+					o.TtoW1=(worldTangent.y,worldBinormal.y,worldNormal.y,worldPos.y);
+					o.TtoW2=(worldTangent.z,worldBinormal.z,worldNormal.z,worldPos.z);
+
+					return o;
+				}
+
+				fixed4 frag(v2f i):SV_Target{
+					float3 worldPos=(i.TtoW0.w,i.TtoW1.w,i.TtoW2.w);
+					fixed3 worldViewDir=normalize(UnityWorldSpaceViewDir(worldPos));
+
+					fixed3 bump=UnpackNormal(tex2D(_BumpTex,i.uv.zw));
+
+					float2 offset=bump*_Distortion*_RefractionTex_TexelSize.xy;
+					i.scrPos.xy=offset+i.scrPos.xy;
+					fixed3 refrColor=tex2D(_RefractionTex,i.scrPos.xy/i.scrPos.w).rgb;
+
+					bump=normalize(half3(dot(i.TtoW0.xyz,bump),dot(i.TtoW1.xyz,bump),dot(i.TtoW2.xyz,bump)));
+					fixed3 reflectDir=reflect(-worldViewDir,bump);
+					fixed4 texColor=tex2D(_MainTex,i.uv.xy);
+					fixed3 reflColor=texCUBE(_CubeMap,reflectDir).rgb*texColor.rgb;
+
+					fixed3 finalColor=reflColor*(1-_RefractAmount)+refrColor*_RefractAmount;
+
+					return fixed4(finalColor,1.0);
+
+				}
+			ENDCG
+		}
+	}
+	FallBack "Tranparent/VertexLit"
+}
