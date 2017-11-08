3aff03b648383694d4f6af31f756b9226878324d
bo_zhang <bo_zhang1993@163.com>
2017-11-08 18:33:00 +0800

    Add FogWithDepthTexture

---
+++ UnityShaderStudy/Assets/Scripts/Chapter13/Chapter13_FogWithDepthTexture.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter13_FogWithDepthTexture :PostEffectsBase
+{
+
+    public Shader fogWithDepthShader;
+    private Material fogMaterial;
+
+    public Material material
+    {
+        get
+        {
+            fogMaterial = CheckShaderAndCreateMaterial(fogWithDepthShader, fogMaterial);
+            return fogMaterial;
+        }
+    }
+
+    private Camera myCamera;
+    public Camera camera
+    {
+        get
+        {
+            if (myCamera == null)
+                myCamera = GetComponent<Camera>();
+            return myCamera;
+        }
+    }
+
+    private Transform myTransform;
+
+    public Transform cameraTransform
+    {
+        get
+        {
+            if (myTransform == null)
+                myTransform = camera.transform;
+            return myTransform;
+        }
+    }
+
+    //定义模拟雾效的参数
+    [Range(0.0f, 3.0f)]
+    public float fogDensity = 1.0f;
+
+    public Color fogColor = Color.white;
+    public float fogStart = 0.0f;
+    public float fogEnd = 2.0f;
+
+    void OnEnable()
+    {
+        camera.depthTextureMode |=DepthTextureMode.Depth;
+    }
+
+    void OnRenderImage(RenderTexture src,RenderTexture dest)
+    {
+        if (material != null)
+        {
+            Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+            float fov = camera.fieldOfView;
+            float near = camera.nearClipPlane;
+            float far = camera.farClipPlane;
+            float aspect = camera.aspect;
+
+            float halfHeight = near*Mathf.Tan(fov*0.5f*Mathf.Deg2Rad);
+            Vector3 toTop = cameraTransform.up*halfHeight;
+            Vector3 toRight = cameraTransform.right*halfHeight*aspect;
+
+            Vector3 topLeft = cameraTransform.forward*near + toTop - toRight;
+            float scale = topLeft.magnitude/near;
+            topLeft.Normalize();
+            topLeft *= scale;
+
+            Vector3 topRight = cameraTransform.forward*near + toTop + toRight;
+            topRight.Normalize();
+            topRight *= scale;
+
+            Vector3 bottomLeft = cameraTransform.forward*near - toTop - toRight;
+            bottomLeft.Normalize();
+            bottomLeft *= scale;
+
+            Vector3 bottomRight = cameraTransform.forward*near - toTop + toRight;
+            bottomRight.Normalize();
+            bottomRight *= scale;
+
+            frustumCorners.SetRow(0, bottomLeft);
+            frustumCorners.SetRow(1, bottomRight);
+            frustumCorners.SetRow(2, topRight);
+            frustumCorners.SetRow(3, topLeft);
+
+            material.SetMatrix("_FrustumCornerRay", frustumCorners);
+          
+            material.SetFloat("_FogDensity", fogDensity);
+            material.SetColor("_FogColor", fogColor);
+            material.SetFloat("_FogStart", fogStart);
+            material.SetFloat("_FogEnd", fogEnd);
+
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            Graphics.Blit(src,dest);
+        }
+    }
+}
---
+++ UnityShaderStudy/Assets/Shader/Chapter13/Chapter13_FogWithDepthTexture.shader
@@ -0,0 +1,85 @@
+﻿Shader "Custom/Chapter13_FogWithDepthTexture" {
+	Properties{
+		_MainTex("MainTex",2D)="white"{}
+		_FogDensity("Fog Density",Float)=1.0
+		_FogColor("FogColor",Color)=(1,1,1,1)
+		_FogStart("Fog Start",Float)=0.0
+		_FogEnd("FogEnd",Float)=2.0
+	}
+	SubShader{
+		CGINCLUDE
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			half4 _MainTex_TexelSize;
+			sampler2D _CameraDepthTexture;
+			half _FogDensity;
+			fixed4 _FogColor;
+			float _FogStart;
+			float _FogEnd;  
+
+			float4x4 _FrustumCornerRay;
+
+			struct v2f{
+				float4 pos:SV_POSITION;
+				half2 uv:TEXCOORD0;
+				half2 uv_depth:TEXCOORD1;
+				float4 interpolatedRay:TEXCOORD2;
+			};
+
+			v2f vert(appdata_img v){
+				v2f o;
+				o.pos=UnityObjectToClipPos(v.vertex);
+				o.uv=v.texcoord;
+				o.uv_depth=v.texcoord;   
+				#if UNITY_UV_STARTS_AT_TOP
+				if(_MainTex_TexelSize.y<0)
+					o.uv_depth.y=1-o.uv_depth.y;
+				#endif
+
+				int index=0;
+				if(v.texcoord.x<0.5&&v.texcoord.y<0.5){
+					index=0;
+				}
+				else if(v.texcoord.x>0.5&&v.texcoord.y<0.5){
+					index=1;
+				}
+				if(v.texcoord.x>0.5&&v.texcoord.y>0.5){
+					index=2;
+				}
+				else{
+					index=3;
+				}
+				#if UNITY_UV_STARTS_AT_TOP
+				if(_MainTex_TexelSize.y<0){
+					index=3-index;
+				}
+				#endif
+				o.interpolatedRay=_FrustumCornerRay[index];
+				return o;
+			}
+
+			fixed4 frag(v2f i):SV_Target{
+				float linearDepth=LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture,i.uv_depth));
+				float3 worldPos=_WorldSpaceCameraPos+linearDepth*i.interpolatedRay.xyz;
+
+				float fogDensity=(_FogEnd-worldPos.y)/(_FogEnd-_FogStart);
+				fogDensity=saturate(fogDensity*_FogDensity);
+
+				fixed4 finalColor=tex2D(_MainTex,i.uv);
+				finalColor.rgb=lerp(finalColor.rgb,_FogColor.rgb,fogDensity);
+				return finalColor;
+			}
+		ENDCG
+
+		Pass{
+			ZTest Always ZWrite Off Cull Off
+			
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+	FallBack Off
+}
