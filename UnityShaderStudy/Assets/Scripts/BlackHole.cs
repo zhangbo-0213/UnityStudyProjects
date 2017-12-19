dd3336c30e9cd37268e7c94a78cc6e23fe8db93d
bo_zhang <bo_zhang1993@163.com>
2017-12-19 20:58:54 +0800

    Add BlackHole

---
+++ UnityShaderStudy/Assets/Scripts/BlackHole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+
+    public Transform blackHole;
+    private Material mat;
+	// Use this for initialization
+	void Start ()
+	{
+	    mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mat.SetVector("_BlackHolePos",blackHole.position);
+	}
+}
